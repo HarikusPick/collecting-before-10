46e73285547fa2260b334905dbf78cf89502ee31
Yunus Furkan Özer <bardakta.kahve@gmail.com>
2022-10-13 22:28:46 +0300

    add

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            for (int i = 1; i <= 10;i++)
+            {
+                toplam += i;
+            }
+            Console.WriteLine(toplam);
+            Console.ReadKey();
+           
+        }
+    }
+}
